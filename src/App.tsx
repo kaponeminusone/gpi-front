da7807b1c43750f7ab98db0574bfebdcf815e51f
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-10-26 17:22:40 -0500

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { AnimatedText } from './components/AnimatedText/AnimatedText'
 import ScrollContent from './components/ScrollContent/ScrollContent'
 import { VideoPlayer } from './components/VideoPlayer/VideoPlayer'
 
@@ -11,10 +12,12 @@ function App() {
       <h1 className='center text-4xl'> Header Hello World!</h1>
       <ScrollContent>
         <div className="bg-primary text-primary-foreground p-8 h-screen flex flex-col items-center justify-center">
-        <VideoPlayer
-          videoUrl="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_1MB.mp4"
-          title="Sample Video"
-          poster="https://images.unsplash.com/photo-1485846234645-a62644f84728?auto=format&fit=crop&q=80&w=1280"
+        <AnimatedText
+          text="Left to Right Animation"
+          type="leftToRight"
+          delay={0.2}
+          duration={0.8}
+          className="text-4xl font-bold"
         />
         </div>
         <div className="bg-secondary text-secondary-foreground p-8 h-screen flex flex-col items-center justify-center">
